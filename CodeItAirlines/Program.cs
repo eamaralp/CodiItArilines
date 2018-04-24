2b993e2ea1e672d141a08c36f9a49aac3e2b770d
eduardo.pereira <eduardoamaralp@gmail.com>
2018-04-24 19:14:35 -0300

    Commit inicial do projeto

---
+++ CodeItAirlines/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeItAirlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
